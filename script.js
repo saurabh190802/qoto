68c5b969b1ecabea234f77c393e3d2b88587c2d1
saurabh satapathy <saurabhsatapathy0@gmail.com>
2022-05-22 14:20:37 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+fetch("http://api.quotable.io/random")
+  .then((data) => data.json())
+  .then((quoteData) => {
+    const quoteText = quoteData.content;
+    const quoteElement = document.getElementById("quoteElement");
+    
+    quoteElement.innerHTML = quoteText;
+  });
